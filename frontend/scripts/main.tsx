6e650c18ace27f307a71fb07128087f00146c005
sungwoo00 <dhdpdhdp5899@gmail.com>
2025-01-13 23:46:25 +0900

    [실습] JSX 변환

---
+++ frontend/scripts/main.tsx
@@ -0,0 +1,21 @@
+import React from '../lib/react.js';
+import ReactDOM from '../lib/react-dom/client.js';
+import App from './app.tsx';
+
+// console.log('React 객체:', React);
+
+// console.log('createElement 메서드:', React.createElement);
+
+const root = document.getElementById('react');
+
+if (!root) {
+  alert('문서에 #react 요소가 존재하지 않습니다.');
+} else {
+  // console.log('App 컴포넌트:', App);
+  // console.log('JSX:', <App />);
+  // console.log('createElement:', React.createElement(App));
+  // const app = React.createElement(App);
+  // const app = <App></App>;
+  // const app = <App />;
+  ReactDOM.createRoot(root).render(<App />);
+}
