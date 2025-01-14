7be6a3f22aaa816065fe52bb47010797dfcd7d31
sungwoo00 <dhdpdhdp5899@gmail.com>
2025-01-14 09:35:09 +0900

    [질문 답변] 17버전이후 jsx 처리
    - jsx() 함수
    - createElement() 함수와 사용법이 유사 (같지는 않음)
    - React 엘리먼트 생성
    - 컴파일 환경에서 자동(automatic) 주입 됨

--- frontend/scripts/app.tsx
+++ frontend/scripts/app.tsx
@@ -1,6 +1,11 @@
 import React from '../lib/react.js';
 import Switch from '../components/switch.tsx';
 
+//17버전 이전과 이후 jsx 처리 확인 코드
+// import jsxRuntime from '../lib/react/jsx-runtime.js';
+
+// const { jsx } = jsxRuntime;
+
 const appStyles = {
   display: 'flex',
   flexFlow: 'column',
@@ -21,6 +26,19 @@ function App(): React.ReactNode {
       <Switch disabled onToggle={handleSwitch3Toggle}>
         Reduced Motion
       </Switch>
+      //17버전 이전과 이후 jsx 처리 확인
+      {/* <ul className='jsx-ul'>
+        <li>child 1</li>
+        <li>child 2</li>
+        <li>child 3</li>
+      </ul>
+
+      <br />
+
+      {jsx('ul', {
+        className: 'jsx-ul',
+        children: [jsx('li', { children: 'child 1' }, 'child-1'), jsx('li', { children: 'child 2' }, 'child-2'), jsx('li', { children: 'child 3' }, 'child-3')],
+      })} */}
     </div>
   );
 }
