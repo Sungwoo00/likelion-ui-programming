55db46aa3d7a973e2b021c20b0a9936885f598d7
sungwoo00 <dhdpdhdp5899@gmail.com>
2025-01-15 22:00:09 +0900

    [실습] 순수 함수
    - 순수 함수(함수의 실행 결과 예측 가능한 함수)
    - 순수 함수, 순수하지 않은 함수 예시 코드
    - strict mode(순수함수 체크)

--- frontend/scripts/main.tsx
+++ frontend/scripts/main.tsx
@@ -1,6 +1,6 @@
 import React from '../lib/react.js';
 import ReactDOM from '../lib/react-dom/client.js';
-import App from './app.tsx';
+import Playground from '../components/playground.tsx';
 
 // console.log('React 객체:', React);
 
@@ -11,11 +11,16 @@ const root = document.getElementById('react');
 if (!root) {
   alert('문서에 #react 요소가 존재하지 않습니다.');
 } else {
-  // console.log('App 컴포넌트:', App);
-  // console.log('JSX:', <App />);
-  // console.log('createElement:', React.createElement(App));
-  // const app = React.createElement(App);
-  // const app = <App></App>;
-  // const app = <App />;
-  ReactDOM.createRoot(root).render(<App />);
+  ReactDOM.createRoot(root).render(
+    // console.log('App 컴포넌트:', App);
+    // console.log('JSX:', <App />);
+    // console.log('createElement:', React.createElement(App));
+    // const app = React.createElement(App);
+    // const app = <App></App>;
+    // const app = <App />;
+
+    <React.StrictMode>
+      <Playground />
+    </React.StrictMode>
+  );
 }
